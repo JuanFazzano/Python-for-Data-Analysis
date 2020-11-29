6f04e61d991596f4d479eeea248df1c8b9382d28
Juan Fazzano <39809282+JuanFazzano@users.noreply.github.com>
2020-11-29 02:47:49 -0300

    Update fancyIndex.py

--- Chapter 4/1 The NumPy ndarray/fancyIndex.py
+++ Chapter 4/1 The NumPy ndarray/fancyIndex.py
@@ -9,16 +9,16 @@ arr = np.array([
 ['o0','o1','o2','o3','o4'],
 ['u0','u1','u2','u3','u4']])
 
-fancyIndex = [1,3]
-print(arr[fancyIndex],'\n')
+fancy_index = [1,3]
+print(arr[fancy_index],'\n')
 # [['e0' 'e1' 'e2' 'e3' 'e4']
 #  ['o0' 'o1' 'o2' 'o3' 'o4']]
 
-fancyIndex = [-1,-3]
-print(arr[fancyIndex],'\n')
+fancy_index = [-1,-3]
+print(arr[fancy_index],'\n')
 # [['u0' 'u1' 'u2' 'u3' 'u4']
 #  ['i0' 'i1' 'i2' 'i3' 'i4']]
 
-fancyIndex = [0,2,4],[1,3,4]
-print(arr[fancyIndex],'\n')
+fancy_index = [0,2,4],[1,3,4]
+print(arr[fancy_index],'\n')
 # ['a1' 'i3' 'u4']
