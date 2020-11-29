56c1b3469f63400a3206e878ee7ac058a08b6b13
Juan Fazzano <39809282+JuanFazzano@users.noreply.github.com>
2020-11-29 02:43:30 -0300

    Create basic_indexing_with_slices.py

---
+++ Chapter 4/1 The NumPy ndarray/basic_indexing_with_slices.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+'''
+[[0,0,0],
+[0,0,0],
+[0,0,0]]
+'''
+
+lado = 3
+arr = np.zeros([lado, lado ]) #([largo, ancho])
+print(arr,'\n')
+
+'''
+[[0,1,1],
+[0,1,1],
+[0,0,0]]
+'''
+
+arr1 = arr.copy()
+arr1[:2,1:]=1
+print(arr1,'\n')
+
+'''
+[[0,0,0],
+[0,0,0],
+[1,1,1]]
+'''
+
+arr2 = arr.copy()
+arr2[-1,:]=1
+print(arr2,'\n')
+
+'''
+[[1,1,0],
+[1,1,0],
+[1,1,0]]
+'''
+
+arr3 = arr.copy()
+arr3[:,:-1]=1
+print(arr3,'\n')
+
+'''
+[[0,0,0],
+[1,1,0],
+[0,0,0]]
+'''
+
+arr4 = arr.copy()
+arr4[1:-1,:-1]=1
+print(arr4,'\n')
